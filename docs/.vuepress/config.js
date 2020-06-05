17c3d3d4807eb5311cc5901062a811cf8a88d10e
xlfsummer <xlf-summer@outlook.com>
2020-06-06 00:58:37 +0800

    使用 vuepress 搭建基础结构

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Shell Doc',
+    description: '在线命令行文档',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '教程', link: '/guide/' },
+            { text: '示例', link: '/examples/' },
+        ],
+        sidebar: {
+            '/guide/': [
+                '',
+                '介绍',
+                '文件导航',
+            ],
+        }
+    }
+}
